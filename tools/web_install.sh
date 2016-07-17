b9cda423ff847c5748a5530ecc56562d87302bec
Nick Zhang <thesadboy@qq.com>
2016-07-17 08:37:01 +0800

    update

--- tools/web_install.sh
+++ tools/web_install.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
+echo "[NOTICE] Clear old files..."
 rm -rf /tmp/merlin-web*
-echo "Download zip file..."
+echo "[NOTICE] Download zip file..."
 wget https://github.com/thesadboy/merlin-web-for-r6900/raw/master/tools/merlin-web.zip -P /tmp/ --no-check-certificate
-echo "Unzip zip file"
+echo "[NOTICE] Unzip zip file"
 unzip /tmp/merlin-web.zip -d /tmp/
-echo "copy /www to /jffs/merlin-web"
+echo "[NOTICE] copy /www to /jffs/merlin-web"
 cp -r /www /jffs/merlin-web
-echo "override /jffs/merlin-web with downloaded files"
+echo "[NOTICE] Override /jffs/merlin-web with downloaded files"
 cp -r /tmp/merlin-web/* /jffs/merlin-web
 if [ ! -f "/jffs/scripts/init-start" ]
 then
-  echo "create /jffs/scripts/init-start and add startup cmd"
+  echo "[NOTICE] Create /jffs/scripts/init-start and add startup cmd"
   echo "!/bin/sh" >> /jffs/scripts/init-start
   echo "mount --bind /jffs/merlin-web /www" >> /jffs/scripts/init-start
   chmod 777 /jffs/scripts/init-start
 else
-  echo "add startup cmd into /jffs/scripts/init-start"
+  echo "[NOTICE] Add startup cmd into /jffs/scripts/init-start"
   echo "mount --bind /jffs/merlin-web /www" >> /jffs/scripts/init-start
 fi
+echo "[NOTICE] Restart router..."

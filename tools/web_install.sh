7c79041f979fc513c10af31b43c0bdcf103603dd
Nick Zhang <thesadboy@qq.com>
2016-07-17 08:24:08 +0800

    update

---
+++ tools/web_install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "Download zip file..."
+wget https://github.com/thesadboy/merlin-web-for-r6900/raw/master/tools/merlin-web.zip -P /tmp/ --no-check-certificate
+echo "Unzip zip file"
+unzip /tmp/merlin-web.zip -d /tmp/
+echo "copy /www to /jffs/merlin-web"
+cp -r /www /jffs/merlin-web
+echo "override /jffs/merlin-web with downloaded files"
+cp -r /tmp/merlin-web/* /jffs/merlin-web
+if [ ! -f "/jffs/scripts/init-start" ]
+then
+  echo "add startup cmd into /jffs/scripts/init-start"
+  echo "mount --bind /jffs/merlin-web /www" >> /jffs/scripts/init-start
+else
+  echo "create /jffs/scripts/init-start and add startup cmd"
+  echo "!/bin/sh" >> /jffs/scripts/init-start
+  echo "mount --bind /jffs/merlin-web /www" >> /jffs/scripts/init-start
+  chmod 777 /jffs/scripts/init-start
+fi
